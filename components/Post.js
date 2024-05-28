0dee09a776b781baabc86532f05e9ae3c023e5fc
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-05-28 13:48:49 -0400

    add changes

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export default function Post({ post }) {
   return (
     <div className='card'>
-      <img src={post.frontmatter.cover_image} alt='' href={`${post.frontmatter.externalLink}`} passHref target='_blank'/>
+      <Link href={`${post.frontmatter.cover_image}`} target='_blank'>
+      <img src={post.frontmatter.cover_image} alt='' />
+      </Link>
 
       <div className='post-date'>Posted on {post.frontmatter.date}</div>
 
@@ -9,9 +13,9 @@ export default function Post({ post }) {
 
       <p>{post.frontmatter.excerpt}</p>
 
-      <Link href={`/blog/${post.slug}`} passHref>
+      <Link href={`/blog/${post.slug}`}>
         <a className='btn'>Read More</a>
       </Link>
     </div>
-  );
+  )
 }
