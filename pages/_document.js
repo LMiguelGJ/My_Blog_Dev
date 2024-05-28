534acaec937795819636ef93725bfac3dbe82ff3
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-05-27 23:10:54 -0400

    add more banner

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            type="text/javascript"
+            src="//pl23421167.highcpmgate.com/3a/82/3b/3a823bbfba5905f487eba3e930e7f4b7.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            type="text/javascript"
+            src="//pl23422628.highcpmgate.com/31/24/84/312484316662331a3d8f85e715e08430.js"
+          ></script>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
