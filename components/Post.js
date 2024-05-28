5f668fca6f387f2cb346e16c47238dce711b504b
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-05-28 13:34:12 -0400

    add changes

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,7 @@
 export default function Post({ post }) {
-  const handleReadMoreClick = (e) => {
-    e.preventDefault();
-    window.open(post.frontmatter.externalLink, '_blank');
-    window.location.href = `/blog/${post.slug}`;
-  };
-
   return (
     <div className='card'>
-      <img src={post.frontmatter.cover_image} alt='' onClick={handleReadMoreClick}/>
+      <img src={post.frontmatter.cover_image} alt='' href={`${post.frontmatter.externalLink}`} passHref target='_blank'/>
 
       <div className='post-date'>Posted on {post.frontmatter.date}</div>
 
@@ -15,7 +9,9 @@ export default function Post({ post }) {
 
       <p>{post.frontmatter.excerpt}</p>
 
-      <a className='btn' href={`/blog/${post.slug}`} passHref>Read More</a>
+      <Link href={`/blog/${post.slug}`} passHref>
+        <a className='btn'>Read More</a>
+      </Link>
     </div>
   );
 }
