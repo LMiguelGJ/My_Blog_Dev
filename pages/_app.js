334e04ca7aadba8bd11e0cd877f3f589d8fd387a
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-06-03 06:02:55 -0400

    remove all banner and ads

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Header from '../components/Header';
 import '../styles/globals.css';
 import PopunderScript from '../components/PopunderScript';
-import SocialBar from '../components/SocialBar';
-import Banner from '../components/Banner';
-import BannerHead from '../components/BannerHead';
+// import SocialBar from '../components/SocialBar';
+// import Banner from '../components/Banner';
+// import BannerHead from '../components/BannerHead';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -14,23 +14,23 @@ function MyApp({ Component, pageProps }) {
         }} />
       <script async src="//waust.at/t.js"></script>
       <Header />
-      <SocialBar uniqueKey="SocialBar-Head" />
+      {/* <SocialBar uniqueKey="SocialBar-Head" /> */}
       <PopunderScript uniqueKey="Banner-Head" />
-        <div className='Banner-Head'>
+        {/* <div className='Banner-Head'>
           <BannerHead count={1} prefix="left" />
-        </div>
+        </div> */}
       <main className='container'>
       <PopunderScript uniqueKey="Banner-Left" />
-        <div className='Banner-Left'>
+        {/* <div className='Banner-Left'>
           <Banner count={1} prefix="left" />
-        </div>
+        </div> */}
         <div className='container-post'>
           <Component {...pageProps} />
         </div>
         <PopunderScript uniqueKey="Banner-Right" />
-        <div className='Banner-Right'>
+        {/* <div className='Banner-Right'>
           <Banner count={1} prefix="right" />
-        </div>
+        </div> */}
       </main>
     </>
   );
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -3,7 +3,7 @@ import path from 'path'
 import matter from 'gray-matter'
 import marked from 'marked'
 import Link from 'next/link'
-import SocialBar from '../../components/SocialBar';
+// import SocialBar from '../../components/SocialBar';
 
 
 export default function PostPage({
@@ -16,7 +16,7 @@ export default function PostPage({
       <Link href='/'>
         <a className='btn btn-back'>Go Back</a>
       </Link>
-      <SocialBar uniqueKey="SocialBar-Head" />
+      {/* <SocialBar uniqueKey="SocialBar-Head" /> */}
       <div className='card card-page'>
         <h1 className='post-title'>{title}</h1>
         <div className='post-date'>Posted on {date}</div>
